abfca2f5368d5f3da546e8942c45678af81e32dc
Jingnen <osjn@outlook.com>
2018-11-09 15:52:20 +0800

    第10章探究服务的源码更新完毕

---
+++ 10_ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -0,0 +1,79 @@
+package com.example.servicetest;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    private static final String MS_CHANNEL_ID = "ms_channel_id";
+    private static final CharSequence MS_CHANNEL_NAME = "ms_channel_name";
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    private static final String TAG = "MyService";
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(MS_CHANNEL_ID,
+                    MS_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+
+        Notification notification = new NotificationCompat.Builder(this, MS_CHANNEL_ID)
+                .setContentTitle("This is content title")
+                .setContentText("This content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        startForeground(1, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: executed");
+    }
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d(TAG, "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress executed");
+            return 0;
+        }
+    }
+}
