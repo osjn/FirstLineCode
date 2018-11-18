932ce7825f5d8234a98bd4f71a7b6c27846d8679
Jingnen <osjn@outlook.com>
2018-11-18 22:09:45 +0800

    增加启动页面

---
+++ CoolWeather/app/src/main/java/com/coolweather/android/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.coolweather.android;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Integer time = 2000;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, time);
+    }
+}
